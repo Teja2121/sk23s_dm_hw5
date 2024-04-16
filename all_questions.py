4639e7b263370dc95fe6cd0d40f7c5823866649a
Teja2121 <56876637+Teja2121@users.noreply.github.com>
2024-04-15 21:45:10 -0400

    all_questions.py

--- all_questions.py
+++ all_questions.py
@@ -61,8 +61,7 @@ def question2():
     # forumula in terms of p is: (1/2) * (ln((1-p)/p)) = 0.5 * ln((1-0.3)/0.3) = 0.4236489301936019
     #p = 0.3
     # proper expression on python is ((1/2)) * math.log((1 - p) / p))
-    p = ((1/2) * math.log((1 - p) / p))
-    answers['(c) Weight update'] = eval(p)
+    answers['(c) Weight update'] = '((1/2) * math.log((1 - p) / p))'
     print(answers['(c) Weight update'])
     # type: float
     # the answer should be correct to 3 significant digits
